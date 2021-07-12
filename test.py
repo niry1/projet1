cf71ea286f04836b0b8b6155dc9e520e4e9c5a8c
niry1 <niry.hoareau@gmail.com>
2021-07-12 14:22:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello")
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+f.close()
